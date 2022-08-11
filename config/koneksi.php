28e03a701a824ea7f3ecac0590d6541374c45e90
ridped <110700168+ridped@users.noreply.github.com>
2022-08-11 10:05:31 +0700

    Nembak :v
    Gtw

--- config/koneksi.php
+++ config/koneksi.php
@@ -11,3 +11,4 @@ $domain_without_p = "api.ridped.com"; // sesuaikan dengan domainmu contoh : cont
 $safelinku = "https://adsafelink.com/st?api=9b742daf3abde74d8a4a90fdc47f8eeb14ffd8d1"; // api safelinku
 $key_waysender = "a4d78b25ef8b76ee7e7f6af443ca120f2475905a"; // key wapi
 $sender = "6285872054160"; // sender wapi
+$api_nembak = "https://server-api.waysender.com:8855"; // yagitu aja :v
